c4e13ebb5a395e14b39bd611ffcae61953b7a744
David <Davidchincharashvili@gmail.com>
2021-05-29 15:39:31 +0400

    init

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let sp = new Spreadsheet("https://docs.google.com/spreadsheets/d/e/2PACX-1vTOr60pbD4Xy2CyXocMTXM-B6wYvO5YaRYd6W-3TMgr8pEQEJ4IrPjDvVbHARkm428S_zGIHbXAjoNf/pub?gid=0&single=true&output=csv");
+
+console.log(sp)
+sp.titles = ['date','event','book']
+console.log(sp.json())
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        events: sp.json()
+    }
+  })
---
+++ js/spreadsheet.js
@@ -0,0 +1,46 @@
+class Spreadsheet {
+    constructor(url){
+        this.output = url.match('output=(.+)')[1]
+        this.url = url
+        this.data = axios({method: "GET", url: url})
+        this.titles = [];
+    }
+
+    _zip (keys, values){
+        var result = {};
+        keys.forEach((key, i) => result[key] = values[i]);
+        return result;
+    };
+
+    json(){
+        let return_json = [];
+        this.data.then(response=>{
+            let text = response.data.replace("\n+","\n")
+            let rows = response.data.split("\n")
+            rows.forEach(row => {
+                if  (this.titles) {
+                    let oneRow = this._zip(this.titles,row.split(","));
+                    return_json.push(oneRow);
+                } else {
+                    return_json.push(row.split(","))
+                }
+            });
+        })
+        return return_json;
+    }
+
+    text(){
+        var return_text = "";
+        this.data.then((response)=>{
+            return_text += response.data
+            // console.log(response.data)
+        })
+        return return_text;
+    }
+
+    setTitles(titles){
+        this.titles = titles
+    }
+}
+
+
