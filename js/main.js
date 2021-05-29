1db183090aebd3c00b635328161c2927a9c01b39
David <Davidchincharashvili@gmail.com>
2021-05-29 20:37:44 +0400

    added links to bible books todo: rewrite links

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 let sp = new Spreadsheet("https://docs.google.com/spreadsheets/d/e/2PACX-1vTOr60pbD4Xy2CyXocMTXM-B6wYvO5YaRYd6W-3TMgr8pEQEJ4IrPjDvVbHARkm428S_zGIHbXAjoNf/pub?gid=0&single=true&output=csv");
 
 console.log(sp)
-sp.titles = ['date','event','book']
+sp.titles = ['date','event','book', 'link']
 console.log(sp.json())
 
 var app = new Vue({
